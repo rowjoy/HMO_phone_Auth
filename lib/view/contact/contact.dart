77e9d545b341ef1f87096198535f1ad875809fb9
Jamirul islam (Joy) <69602585+rowjoy@users.noreply.github.com>
2022-01-16 17:04:36 +0600

    update

--- lib/view/contact/contact.dart
+++ lib/view/contact/contact.dart
@@ -1,8 +1,10 @@
 // ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, sized_box_for_whitespace
 
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:hmo/Map/contactdetiles.dart';
 import 'package:hmo/utils/colors.dart';
-import 'package:hmo/view/contact/contactdetails.dart';
+import 'package:hmo/view/contact/contactedit.dart';
 import 'package:hmo/view/contact/newcontactadd.dart';
 import 'package:hmo/widget/drawer/drawer.dart';
 
@@ -61,126 +63,132 @@ class _ContactpageState extends State<Contactpage> {
             itemCount: 5,
             itemBuilder: (context, index) {
               return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ListTile(
-                  selected: resknumber,
-                  onTap: () {
-                    showModalBottomSheet(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(10),
-                          topRight: Radius.circular(10),
-                        )),
-                        context: context,
-                        builder: (contex) {
-                          return Container(
-                            height: 160,
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Column(
-                                children: [
-                                  Container(
-                                    height: 6,
-                                    width: 50,
-                                    decoration: BoxDecoration(
-                                      color:
-                                          Color(COLOR.coustomColors('#DBDBDB')),
-                                      borderRadius: BorderRadius.circular(5),
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    height: 10,
-                                  ),
-                                  ListTile(
-                                    selected: false,
-                                    onTap: () {},
-                                    tileColor:
-                                        Color(COLOR.coustomColors('F6F6F6')),
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(10),
-                                    ),
-                                    leading: Icon(Icons.phone),
-                                    title: Text(
-                                      'Call',
-                                      style: TextStyle(
-                                        fontSize: 17,
-                                      ),
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    height: 5,
-                                  ),
-                                  ListTile(
-                                    selected: false,
-                                    onTap: () {},
-                                    tileColor:
-                                        Color(COLOR.coustomColors('F6F6F6')),
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(10),
-                                    ),
-                                    leading: Icon(Icons.comment),
-                                    title: Text(
-                                      'SMS',
-                                      style: TextStyle(
-                                        fontSize: 17,
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          );
-                        });
-                  },
-                  onLongPress: () {},
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  selectedTileColor: Color(COLOR.coustomColors('#FF4C4C')),
-                  tileColor: Color(COLOR.coustomColors('F6F6F6')),
-                  leading: CircleAvatar(
-                    backgroundColor: Colors.white,
-                    child: Center(
-                      child: Icon(
-                        Icons.person,
-                      ),
+                padding: const EdgeInsets.only(
+                    top: 8, bottom: 8, left: 10, right: 10),
+                child: Slidable(
+                  key: const ValueKey(0),
+                  endActionPane:
+                      ActionPane(motion: const ScrollMotion(), children: [
+                    SlidableAction(
+                      onPressed: (value) {},
+                      backgroundColor: Color(0xFFFE4A49),
+                      foregroundColor: Colors.white,
+                      icon: Icons.delete,
+                      label: 'Delete',
                     ),
-                  ),
-                  title: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  ]),
+                  startActionPane: ActionPane(
+                    motion: ScrollMotion(),
                     children: [
-                      Text(
-                        'Contact Name',
-                        style: TextStyle(
-                          fontSize: 14,
-                          color: Color(
-                            COLOR.coustomColors('707070'),
-                          ),
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      Text(
-                        '+8801234657890',
-                        style: TextStyle(
-                          fontSize: 12,
-                          color: Color(
-                            COLOR.coustomColors('747474'),
-                          ),
-                        ),
+                      SlidableAction(
+                        onPressed: (value) {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => Contactedit()));
+                        },
+                        backgroundColor: Color(COLOR.coustomColors('#00B27A')),
+                        foregroundColor: Colors.white,
+                        icon: Icons.edit,
+                        label: 'Edit',
                       ),
                     ],
                   ),
-                  trailing: IconButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => ContactDetails()));
-                      },
-                      icon: Icon(Icons.arrow_forward_ios_outlined)),
+                  child: ListTile(
+                    onTap: () {
+                      showModalBottomSheet(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            topRight: Radius.circular(10),
+                          )),
+                          context: context,
+                          builder: (contex) {
+                            return Container(
+                              height: 160,
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                  children: [
+                                    Container(
+                                      height: 6,
+                                      width: 50,
+                                      decoration: BoxDecoration(
+                                        color: Color(
+                                            COLOR.coustomColors('#DBDBDB')),
+                                        borderRadius: BorderRadius.circular(5),
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    ListTile(
+                                      selected: false,
+                                      onTap: () {},
+                                      tileColor:
+                                          Color(COLOR.coustomColors('F6F6F6')),
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(10),
+                                      ),
+                                      leading: Icon(Icons.phone),
+                                      title: Text(
+                                        'Call',
+                                        style: TextStyle(
+                                          fontSize: 17,
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 5,
+                                    ),
+                                    ListTile(
+                                      selected: false,
+                                      onTap: () {},
+                                      tileColor:
+                                          Color(COLOR.coustomColors('F6F6F6')),
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(10),
+                                      ),
+                                      leading: Icon(Icons.comment),
+                                      title: Text(
+                                        'SMS',
+                                        style: TextStyle(
+                                          fontSize: 17,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          });
+                    },
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5)),
+                    tileColor: Color(COLOR.coustomColors('#F6F6F6')),
+                    leading: Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.white,
+                      ),
+                      child: Icon(Icons.person),
+                    ),
+                    title: Text('Contact Name'),
+                    subtitle: Text('+8801234657890'),
+                    trailing: IconButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => ContactDetiles()));
+                        },
+                        icon: Icon(
+                          Icons.arrow_forward_ios,
+                          color: Colors.black,
+                        )),
+                  ),
                 ),
               );
             },
